b0e7c27ffe4badb841de56d14a9b8c77405af078
RenatoPacaviraG <58761496+RenatoPacaviraG@users.noreply.github.com>
2023-05-19 08:52:16 +0100

    fsoma

--- main.c
+++ main.c
@@ -4,5 +4,12 @@
 main()
 {
     printf("Hello world!\n");
+    int x, y, z;
+    printf("Digite o valor de x: \n");
+    scanf("%d",&x);
+    printf("Digite o valor de y:\n");
+    scanf("%d",&y);
+    z=x+y;
+    printf("O resultado da soma e: %d \n",z);
     return 0;
 }
